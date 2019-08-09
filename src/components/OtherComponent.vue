373d87d6b8f6c9ce7c798e677c94d668d27fd1de
Bertil Ipsen <ipsen.bertil@gmail.com>
2019-08-09 23:47:50 +0200

    playing around with vuex

--- src/components/OtherComponent.vue
+++ src/components/OtherComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ count }}</h2>
    </div>
 </template>
 
@@ -9,6 +10,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed: {
+    count () {
+      return store.state.count
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+Vue.use(Vuex);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
